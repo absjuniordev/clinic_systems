b8bd59a4fc0db73b5c9974c7936b436cc5d21882
Arnaldo Junior <abs.junnior@hotmail.com>
2024-02-01 23:33:17 -0300

    Implementação do AsyncLoader

--- core/lib/src/lab_clinicas_core_config.dart
+++ core/lib/src/lab_clinicas_core_config.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:asyncstate/asyncstate.dart';
+import 'package:fe_lab_clinicas_core/src/loader/lab_clinicas_loader.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
@@ -26,10 +28,18 @@ class LabClinicasCoreConfig extends StatelessWidget {
       bindings: binding,
       pages: [...pages ?? [], ...pagesBuilder ?? []],
       builder: (context, routes, flutterGetItNavObserver) {
-        return MaterialApp(
-          navigatorObservers: [flutterGetItNavObserver],
-          title: title,
-          routes: routes,
+        return AsyncStateBuilder(
+          loader: LabClinicasLoader(),
+          builder: (navigatorObserver) {
+            return MaterialApp(
+              navigatorObservers: [
+                navigatorObserver,
+                flutterGetItNavObserver,
+              ],
+              routes: routes,
+              title: title,
+            );
+          },
         );
       },
     );
