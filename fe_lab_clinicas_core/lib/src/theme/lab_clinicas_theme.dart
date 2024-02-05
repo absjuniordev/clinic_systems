1d3e4b8ed1fabc3d98df59607a48183a538b1f65
Arnaldo Junior <abs.junnior@hotmail.com>
2024-02-05 16:32:31 -0300

    Criação da classe de Tema

---
+++ fe_lab_clinicas_core/lib/src/theme/lab_clinicas_theme.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+final class LabClinicasTheme {
+  static const orangeColor = Color(0xFFFFAE45);
+  static const ligthOrangeColor = Color(0xFFFFEFE9);
+  static const blueColor = Color(0xFF01BDD6);
+  static const ligthGrayColor = Color(0xFFFFEFE9);
+
+  static final _defaultInputerBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(16),
+    borderSide: const BorderSide(color: ligthOrangeColor),
+  );
+
+  static final lightThem = ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: ligthOrangeColor),
+    useMaterial3: true,
+    scaffoldBackgroundColor: ligthOrangeColor,
+    fontFamily: 'Montserrat',
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: ligthOrangeColor,
+      labelStyle: const TextStyle(
+        fontSize: 14,
+        color: orangeColor,
+        fontWeight: FontWeight.w700,
+      ),
+      floatingLabelStyle:
+          const TextStyle(color: blueColor, fontWeight: FontWeight.w600),
+      enabledBorder: _defaultInputerBorder,
+      focusedBorder: _defaultInputerBorder,
+      errorBorder: _defaultInputerBorder.copyWith(
+        borderSide: const BorderSide(color: Colors.red),
+      ),
+    ),
+  );
+}
