832f85b03a946c84e91fcaa9ace9dc68c3a2f894
Arnaldo Junior <abs.junnior@hotmail.com>
2024-02-05 17:51:14 -0300

    Configuração do obscure com o signal

--- fe_lab_clinicas_self_services/lib/src/modules/auth/login/login_controller.dart
+++ fe_lab_clinicas_self_services/lib/src/modules/auth/login/login_controller.dart
@@ -1,1 +1,8 @@
-class LoginController {}
+import 'package:signals_flutter/signals_flutter.dart';
+
+class LoginController {
+  final _obscurePassword = signal(true);
+  bool get obscurePassword => _obscurePassword();
+
+  void passwordToggle() => _obscurePassword.value = !_obscurePassword.value;
+}
--- fe_lab_clinicas_self_services/lib/src/modules/auth/login/login_page.dart
+++ fe_lab_clinicas_self_services/lib/src/modules/auth/login/login_page.dart
@@ -1,5 +1,8 @@
 import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
+import 'package:fe_lab_clinicas_self_service/src/modules/auth/login/login_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+import 'package:signals_flutter/signals_flutter.dart';
 import 'package:validatorless/validatorless.dart';
 
 class LoginPage extends StatefulWidget {
@@ -13,6 +16,7 @@ class _LoginPageState extends State<LoginPage> {
   final emailEC = TextEditingController();
   final passwordEC = TextEditingController();
   final formKey = GlobalKey<FormState>();
+  final controller = Injector.get<LoginController>();
 
   @override
   void dispose() {
@@ -65,13 +69,26 @@ class _LoginPageState extends State<LoginPage> {
                     const SizedBox(
                       height: 24,
                     ),
-                    TextFormField(
-                      obscureText: true,
-                      controller: passwordEC,
-                      validator: Validatorless.required("Senha obrigatorio"),
-                      decoration: const InputDecoration(
-                        label: Text("Password"),
-                      ),
+                    Watch(
+                      (_) {
+                        return TextFormField(
+                          obscureText: controller.obscurePassword,
+                          controller: passwordEC,
+                          validator:
+                              Validatorless.required("Senha obrigatorio"),
+                          decoration: InputDecoration(
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                controller.passwordToggle();
+                              },
+                              icon: controller.obscurePassword
+                                  ? const Icon(Icons.visibility)
+                                  : const Icon(Icons.visibility_off),
+                            ),
+                            label: const Text("Password"),
+                          ),
+                        );
+                      },
                     ),
                     const SizedBox(
                       height: 32,
