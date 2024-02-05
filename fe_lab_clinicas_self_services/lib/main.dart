c657d2ea5174db0419d5019a6b777fd73056ab29
Arnaldo Junior <abs.junnior@hotmail.com>
2024-02-05 14:59:19 -0300

    Add assets

--- fe_lab_clinicas_self_services/lib/main.dart
+++ fe_lab_clinicas_self_services/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
 import 'package:fe_lab_clinicas_self_service/src/binding/lab_clinicas_application_binding.dart';
+import 'package:fe_lab_clinicas_self_service/src/modules/auth/auth_module.dart';
 import 'package:fe_lab_clinicas_self_service/src/pages/splash_page/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
@@ -17,7 +18,13 @@ class LabClinicasSelfServiceApp extends StatelessWidget {
       title: "Lab Clinicas Auto Atendimento",
       binding: LabClinicasApplicationBinding(),
       pagesBuilder: [
-        FlutterGetItPageBuilder(page: (_) => const SplashPage(), path: '/'),
+        FlutterGetItPageBuilder(
+          page: (_) => const SplashPage(),
+          path: '/',
+        ),
+      ],
+      modulos: [
+        AuthModule(),
       ],
     );
   }
---
+++ fe_lab_clinicas_self_services/lib/src/modules/auth/login/login_router.dart
@@ -0,0 +1,20 @@
+import 'package:fe_lab_clinicas_self_service/src/modules/auth/login/login_controller.dart';
+import 'package:fe_lab_clinicas_self_service/src/modules/auth/login/login_page.dart';
+import 'package:fe_lab_clinicas_self_service/src/services/user_login_services.dart';
+import 'package:fe_lab_clinicas_self_service/src/services/user_login_services_impl.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+
+class LoginRouter extends FlutterGetItModulePageRouter {
+  const LoginRouter({super.key});
+
+  @override
+  List<Bind<Object>> get bindings => [
+        Bind.lazySingleton<UserLoginServices>(
+            (i) => UserLoginServicesImpl(userRepository: i())),
+        Bind.lazySingleton((i) => LoginController()),
+      ];
+
+  @override
+  WidgetBuilder get view => (_) => const LoginPage();
+}
