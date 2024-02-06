6df5314ca227bffe58de27169977ed7171fe2c9e
Arnaldo Junior <abs.junnior@hotmail.com>
2024-02-06 12:02:38 -0300

    Finalizaçao da screem Home page

--- fe_lab_clinicas_self_services/lib/src/modules/auth/home/home_page.dart
+++ fe_lab_clinicas_self_services/lib/src/modules/auth/home/home_page.dart
@@ -6,6 +6,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final sizeOf = MediaQuery.sizeOf(context);
     return Scaffold(
       appBar: LabClinicasAppBar(
         actions: [
@@ -26,7 +27,41 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: Container(
+          margin: const EdgeInsets.only(top: 112),
+          padding: const EdgeInsets.all(40),
+          width: sizeOf.width * .8,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+            border: Border.all(
+              color: LabClinicasTheme.orangeColor,
+            ),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                "Bem Vindo",
+                style: LabClinicasTheme.titleStyle,
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              SizedBox(
+                width: sizeOf.width * .8,
+                height: 48,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('INICIAR TERMINAL'),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
