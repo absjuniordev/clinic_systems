16d5bfebbe6bff6b76b21220858b4f37b112da63
Arnaldo Junior <abs.junnior@hotmail.com>
2024-02-06 21:51:49 -0300

    Criação da tela dos pacientes

--- fe_lab_clinicas_self_services/lib/src/modules/self_service/find_patient.dart/find_patient_page.dart
+++ fe_lab_clinicas_self_services/lib/src/modules/self_service/find_patient.dart/find_patient_page.dart
@@ -1,18 +1,118 @@
-import 'package:fe_lab_clinicas_self_service/src/modules/self_service/self_service_controller.dart';
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_getit/flutter_getit.dart';
+import 'package:validatorless/validatorless.dart';
 
-class FindPatientPage extends StatelessWidget {
+class FindPatientPage extends StatefulWidget {
   const FindPatientPage({super.key});
 
   @override
+  State<FindPatientPage> createState() => _FindPatientPageState();
+}
+
+class _FindPatientPageState extends State<FindPatientPage> {
+  final formKey = GlobalKey<FormState>();
+  final documentEC = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    Injector.get<SelfServiceController>().debug();
+    final sizeOf = MediaQuery.sizeOf(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Find patient'),
-      ),
-      body: Container(),
-    );
+        appBar: LabClinicasAppBar(
+          actions: [
+            PopupMenuButton(
+              itemBuilder: (context) {
+                return [
+                  const PopupMenuItem(
+                    value: 1,
+                    child: Text("Reiniciar Processo"),
+                  ),
+                ];
+              },
+              onSelected: (value) async {},
+            )
+          ],
+        ),
+        body: LayoutBuilder(
+          builder: (_, constrains) {
+            return SingleChildScrollView(
+              child: Container(
+                constraints: BoxConstraints(minHeight: sizeOf.height),
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/background_login.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Center(
+                  child: Container(
+                    padding: const EdgeInsets.all(40),
+                    width: MediaQuery.sizeOf(context).width * .8,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16)),
+                    child: Form(
+                      key: formKey,
+                      child: Column(
+                        children: [
+                          Image.asset('assets/images/logo_vertical.png'),
+                          const SizedBox(
+                            height: 48,
+                          ),
+                          TextFormField(
+                            controller: documentEC,
+                            validator:
+                                Validatorless.required("CPF Obrigatorio"),
+                            decoration: const InputDecoration(
+                              label: Text("Digite o CPF do Paciente"),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 24,
+                          ),
+                          Row(
+                            children: [
+                              const Text(
+                                "Não sabe o CPF do Paciente",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: LabClinicasTheme.blueColor,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () {},
+                                child: const Text(
+                                  "Clique aqui",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: LabClinicasTheme.orangeColor,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 24,
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            width: sizeOf.width * .8,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                final valid =
+                                    formKey.currentState?.validate() ?? false;
+                                if (valid) {}
+                              },
+                              child: const Text("CONTINUAR"),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        ));
   }
 }
