6ab28f4e95e96bedf0c26daea657a33f742b60ff
Arnaldo Junior <abs.junnior@hotmail.com>
2024-02-07 10:34:41 -0300

    Criação do layout da page patiente

--- fe_lab_clinicas_self_services/lib/src/modules/self_service/patient/patiente_page.dart
+++ fe_lab_clinicas_self_services/lib/src/modules/self_service/patient/patiente_page.dart
@@ -1,3 +1,5 @@
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
+import 'package:fe_lab_clinicas_self_service/src/modules/self_service/widgets/lab_clinicas_self_service_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class PatientPage extends StatelessWidget {
@@ -5,11 +7,196 @@ class PatientPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final sizeOf = MediaQuery.sizeOf(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Patiente'),
+      appBar: LabClinicasSelfServiceAppBar(),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: SingleChildScrollView(
+          child: Container(
+            margin: const EdgeInsets.only(top: 18),
+            padding: const EdgeInsets.all(32),
+            width: sizeOf.width * .8,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(color: LabClinicasTheme.orangeColor),
+            ),
+            child: Column(
+              children: [
+                Image.asset('assets/images/check_icon.png'),
+                const SizedBox(
+                  height: 24,
+                ),
+                const Text(
+                  "Cadastro encontrado",
+                  style: LabClinicasTheme.titleSmallStyle,
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                const Text(
+                  "Confirme os dados do seu cadastro",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: LabClinicasTheme.blueColor,
+                  ),
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    label: Text("Nome do Paciente"),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    label: Text('Emal'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    label: Text('Telefono de contato'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    label: Text('Digite o seu CPF'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    label: Text('CEP'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Flexible(
+                      flex: 3,
+                      child: TextFormField(
+                        decoration:
+                            const InputDecoration(label: Text('Endereço')),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    Flexible(
+                      child: TextFormField(
+                        decoration:
+                            const InputDecoration(label: Text('Numero')),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration:
+                            const InputDecoration(label: Text('Complemento')),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        decoration:
+                            const InputDecoration(label: Text('Estado')),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration:
+                            const InputDecoration(label: Text('Cidade')),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        decoration:
+                            const InputDecoration(label: Text('Bairro')),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(label: Text('Responsavel')),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                      label: Text('Documento de Identificação')),
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                          height: 48,
+                          child: OutlinedButton(
+                            onPressed: () {},
+                            child: const Text("Editar"),
+                          )),
+                    ),
+                    const SizedBox(
+                      width: 17,
+                    ),
+                    Expanded(
+                      child: SizedBox(
+                          height: 48,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: const Text("Continuar"),
+                          )),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
       ),
-      body: Container(),
     );
   }
 }
