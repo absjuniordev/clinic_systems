c7b89b98891d94fee5b7f49fb50247d03d809b4b
Arnaldo Junior <abs.junnior@hotmail.com>
2024-02-07 10:39:46 -0300

    Criação do form controller

---
+++ fe_lab_clinicas_self_services/lib/src/modules/self_service/patient/patient_form_controller.dart
@@ -0,0 +1,18 @@
+import 'package:fe_lab_clinicas_self_service/src/modules/self_service/patient/patient_page.dart';
+import 'package:flutter/material.dart';
+
+mixin PatientFormController on State<PatientPage> {
+  final nameEC = TextEditingController();
+  final emailEC = TextEditingController();
+  final phoneEC = TextEditingController();
+  final documentEC = TextEditingController();
+  final cepEC = TextEditingController();
+  final streetEC = TextEditingController();
+  final numberEC = TextEditingController();
+  final complementEC = TextEditingController();
+  final stateEC = TextEditingController();
+  final cityEC = TextEditingController();
+  final districtEC = TextEditingController();
+  final guardianEC = TextEditingController();
+  final guardianIdentificationNumberEC = TextEditingController();
+}
