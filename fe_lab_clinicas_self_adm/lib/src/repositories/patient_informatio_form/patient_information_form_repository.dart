d1a55acc57b80aaa61170c8aff2e54620fda92a6
Arnaldo Junior <abs.junnior@hotmail.com>
2024-02-09 18:07:15 -0300

    Definição dos parametros para chamada dos clientes

---
+++ fe_lab_clinicas_self_adm/lib/src/repositories/patient_informatio_form/patient_information_form_repository.dart
@@ -0,0 +1,9 @@
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
+import 'package:fe_lab_clinicas_self_adm/src/models/patiente_information_form_model.dart';
+
+abstract interface class PatientInformationFormRepository {
+  Future<Either<RepositoryException, PatienteInformationFormModel?>>
+      callNextToCheckcin();
+  Future<Either<RepositoryException, Unit>> updateStatus(
+      String id, PatientInformationFormStatus status);
+}
