825aeaa418827242bc935eede40ecaf3b2e72ace
Arnaldo Junior <abs.junnior@hotmail.com>
2024-02-14 16:48:52 -0300

    Importação da tela de Login

--- fe_lab_clinicas_panel/lib/main.dart
+++ fe_lab_clinicas_panel/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
+import 'package:fe_lab_clinicas_panel/src/pages/login/login_router.dart';
 import 'package:fe_lab_clinicas_panel/src/pages/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
+
+import 'src/binding/lab_clinicas_application_binding.dart';
 
 void main() {
   runApp(const LabClinicasPanel());
@@ -14,9 +17,16 @@ class LabClinicasPanel extends StatelessWidget {
   Widget build(BuildContext context) {
     return LabClinicasCoreConfig(
       pagesBuilder: [
-        FlutterGetItPageBuilder(page: (_) => const SplashPage(), path: '/')
+        FlutterGetItPageBuilder(
+          page: (_) => const SplashPage(),
+          path: '/',
+        )
       ],
       title: 'Lab Clinicas Panel',
+      pages: const [
+        LoginRouter(),
+      ],
+      binding: LabClinicasApplicationBinding(),
     );
   }
 }
